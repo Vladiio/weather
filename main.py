cce8eb7e40ffa2671300c80edccf79af27cb31b6
Vladiio <vladpochta5@gmail.com>
2017-04-19 14:00:18 +0300

    add temperature, wind and clouds

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import requests
 
 
 class Weather:
-    def __init__(self, city_id='706483', key_file='api.key',
-                 data_file='weather.info'):
+    def __init__(self, city_id='706483', key_file='.api.key',
+                 data_file='.weather.info'):
         self.city_id = city_id
         self.key_filename = key_file
         self.data_filename = data_file
@@ -13,25 +13,24 @@ class Weather:
                     'data/2.5/weather?id={id}&appid={key}')
         self.info_pattern = '''
             It is {description}.
-            Temperature: {temp}
+            Temperature: {temp}C
             speed of wind: {speed}
-            clouds: {clouds}
+            clouds: {clouds}%
             '''
 
     def sync(self):
         response = requests.get(self.url.format(
                 id=self.city_id, key=self.__get_key()))
         if response.status_code == 200:
-            self.__serialize_data(data)
+            self.__serialize_data(response.content)
             print('synced')
 
     def display(self):
-        data = self.__get_data()
-        description = data['weather'][0]['description']
-        print(self.info_pattern.format(description=description,
-                                       temp='', speed='', clouds=''))
+        data = self.__load_from_json()
+        info = get_info(data)
+        print(self.info_pattern.format(**info))
 
-    def __get_data(self):
+    def __load_from_json(self):
         try:
             size = os.path.getsize(self.data_filename)
         except OSError:
@@ -60,3 +59,13 @@ class Weather:
         with open(self.data_filename, 'rb') as file:
             return pickle.load(file).decode('utf-8')
 
+
+def get_info(data):
+    description = data['weather'][0]['description']
+    temp = data['main']['temp'] / 273.15
+    temp = str(round(temp))
+    wind_speed = str(data['wind']['speed'])
+    clouds = str(data['clouds']['all'])
+    return dict(description=description,
+                temp=temp, speed=wind_speed,
+                clouds=clouds)
